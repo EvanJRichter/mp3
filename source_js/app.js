838d9eaa4579d1a7b999bd156aae53f48a8fb75c
Evan Richter <evan@evanhere.com>
2016-10-09 13:38:34 -0500

    almost done

--- source_js/app.js
+++ source_js/app.js
@@ -1,5 +1,44 @@
+
+//https://code.angularjs.org/1.2.21/docs/guide/services
+
 var app = angular.module('mp3',['ngRoute']);
+	// .service('movieService', function() {
+	// 	var movieList = null;
+
+	// 	$http.get('./data/imdb250.json').
+	// 		success(function(data) {
+	// 	  		movieList = data;
+	// 		}).
+	// 		error(function(err) {
+	// 	  		console.log(err);
+	// 		});
+
+	// 	 return {
+	// 	 	getMovieList: function(){
+	// 	 		return movieList;
+	// 	 	}
+	// 	 };
+	// });
+
 
 app.config(function ($routeProvider) {
-
+    $routeProvider
+        .when('/list', { 
+            templateUrl : 'partials/list.html',
+            controller: 'myListController'
+        })
+        .when('/gallery', {
+            templateUrl : 'partials/gallery.html',
+            controller: 'myGalleryController'
+        })
+        .when('/details/:movie_id', {
+            templateUrl : 'partials/details.html',
+            controller: 'myDetailsController'
+        })
+        .otherwise({
+            redirectTo: '/gallery'
+        });
 })
+
+
+
